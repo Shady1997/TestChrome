46b63b72f3274e3e7724bd86c7cb8aaca903eae1
Shady GOMAA <shadyahmed9719@gmail.com>
2023-02-02 03:08:11 +0200

    create new project

---
+++ src/main/java/testChrome/capabilities.java
@@ -0,0 +1,33 @@
+package testChrome;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class capabilities {
+	
+public static AndroidDriver<AndroidElement> baseCapabilities() throws MalformedURLException {
+		
+		// TODO Declare DesiredCapabilites to automate with android OS
+
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+
+		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "shady");
+
+		capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+
+		capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "chrome");
+		
+		capabilities.setCapability("chromedriverExecutable","C:\\webdrivers\\chromedriver.exe");
+		
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		
+		return driver;
+	}
+
+}
